a43c01d056d5caa5f4c9016bed56c89edeaaf43f
Yesid Marin <yesid@autentikar.com>
2024-02-26 20:36:20 -0500

    version 1.4.10

---
+++ AKCardCaptureSwift.xcframework/ios-arm64/AKCardCaptureSwift.framework/Headers/AKCardCaptureSwift.h
@@ -0,0 +1,18 @@
+//
+//  AKCardCaptureSwift.h
+//  AKCardCaptureSwift
+//
+//  Created by ymarin on 10/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKCardCaptureSDK.
+FOUNDATION_EXPORT double AKCardCaptureSwiftVersionNumber;
+
+//! Project version string for AKCardCaptureSDK.
+FOUNDATION_EXPORT const unsigned char AKCardCaptureSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKCardCaptureSwift/PublicHeader.h>
+
+
